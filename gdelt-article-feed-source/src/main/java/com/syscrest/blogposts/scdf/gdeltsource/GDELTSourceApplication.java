246778788a2956aa3d00c86fccdb8e1b355ddd55
Thomas Memenga <t.memenga@syscrest.com>
2019-10-09 18:44:53 +0200

    changing gdelt api call

--- gdelt-article-feed-source/src/main/java/com/syscrest/blogposts/scdf/gdeltsource/GDELTSourceApplication.java
+++ gdelt-article-feed-source/src/main/java/com/syscrest/blogposts/scdf/gdeltsource/GDELTSourceApplication.java
@@ -9,8 +9,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.reactivestreams.Publisher;
@@ -51,6 +58,8 @@ public class GDELTSourceApplication {
 	@Autowired
 	private GDELTSourceProperties properties;
 
+	DateTimeFormatter START_DATETIME_FORMATTER = DateTimeFormat.forPattern("yyyyMMddHHmmss").withZone(DateTimeZone.UTC);
+
 	@StreamEmitter
 	@Output(Source.OUTPUT)
 	@Bean
@@ -58,9 +67,12 @@ public class GDELTSourceApplication {
 		return IntegrationFlows.from(() -> {
 			try {
 
+				DateTime startDate = new DateTime().withZone(DateTimeZone.UTC).minusMinutes(31);
+
 				URL feedUrl = new URL("https://api.gdeltproject.org/api/v2/doc/doc?query="
 						+ URLEncoder.encode(properties.getQuery(), "UTF-8")
-						+ "&mode=artlist&maxrecords=250&timespan=1h&sort=datedesc&format=json");
+						+ "&mode=artlist&maxrecords=250&startdatetime=" + START_DATETIME_FORMATTER.print(startDate)
+						+ "&sort=HybridRel&format=json");
 				logger.info("going to fetch data from gdeltproject.org using url = " + feedUrl);
 				InputStream inputStreamObject = feedUrl.openStream();
 				BufferedReader streamReader = new BufferedReader(new InputStreamReader(inputStreamObject, "UTF-8"));
@@ -83,6 +95,7 @@ public class GDELTSourceApplication {
 					a.setSeendate(article.getString("seendate"));
 					response.add(a);
 				}
+				logger.info("emitting list with " + response.size() + " articles");
 				return new GenericMessage<>(response);
 			} catch (Throwable e) {
 				logger.error("while querying gdelt endpoint", e);
